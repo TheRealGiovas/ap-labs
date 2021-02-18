50b8cc513a65cb38a2e1b32640ed5dd963386e3b
Katacoda Scenario <scenario@katacoda.com>
2021-02-18 01:49:05 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  
+  var names = ""
+  
+  for i := 1; i < len(os.Args); i++ {
+    names += os.Args[i] + " "
+  }
+  
+	fmt.Println("Welcome to the jungle", names)
 }
